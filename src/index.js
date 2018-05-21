824318bd29344d1f24bcedcc76889fd04cc8f0f3
aSH <ash.uncover@gmail.com>
2018-05-22 00:45:21 +0200

    veresion 0.0.12

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import { ConsoleMock as ConsoleMockBase } from 'mocks/Console.mock.js'
-import { getConsoleMock as getConsoleMockBase } from 'mocks/Console.mock.js'
+import { ConsoleMock as ConsoleMockBase } from './mocks/Console.mock.js'
+import { getConsoleMock as getConsoleMockBase } from './mocks/Console.mock.js'
 export const ConsoleMock = ConsoleMockBase
 export const getConsoleMock = getConsoleMockBase
 
-import { LocalStorageMock as LocalStorageMockBase } from 'mocks/LocalStorage.mock.js'
-import { getLocalStorageMock as getLocalStorageMockBase } from 'mocks/LocalStorage.mock.js'
+import { LocalStorageMock as LocalStorageMockBase } from './mocks/LocalStorage.mock.js'
+import { getLocalStorageMock as getLocalStorageMockBase } from './mocks/LocalStorage.mock.js'
 export const LocalStorageMock = LocalStorageMockBase
 export const getLocalStorageMock = getLocalStorageMockBase
